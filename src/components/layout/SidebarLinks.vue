f32064df91be40979e9802322e740a795748b4bf
mars <marsojane.studio@gmail.com>
2024-07-26 20:46:56 +0800

    components, datatables - shadcn, tanstack

---
+++ src/components/layout/SidebarLinks.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Linkedin } from 'lucide-vue-next'
+
+interface LinkProp {
+  title: string
+  to: string
+  icon: string
+}
+
+defineProps<{
+  links: LinkProp[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    activeClass=""
+    exactActiveClass="text-primary bg-muted"
+    v-for="link in links"
+    :key="link.title"
+    :to="link.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+</template>
